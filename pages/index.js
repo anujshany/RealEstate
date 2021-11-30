b0848d07f3348f0e6ff538335befcb6ebd55b035
Anuj Shany <64464309+anujshany@users.noreply.github.com>
2021-11-30 07:24:32 +0530

    filters added

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home({propertiesForRent,propertiesForSale}) {
         title2="Everyone"
         desc1 = "Explore Apartments, Villas, Homes"
         desc2 = "and more"
-        buttonText = "Start Exploring"
+        buttonText = "Explore Renting"
         linkName="/search?purpose=for-rent"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
       
@@ -47,14 +47,14 @@ export default function Home({propertiesForRent,propertiesForSale}) {
         title2="Dream Home"
         desc1 = "Explore Apartments, Villas, Homes"
         desc2 = "and more"
-        buttonText = "Exploring Buying"
+        buttonText = "Explore Buying"
         linkName="/search?purpose=for-sale"
         imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
           
       />
-      
+      <Flex flexWrap="wrap">
       {propertiesForSale.map((property)=><Property property={property} key={property.id}/>)}
-
+      </Flex>
     </Box>
     )
 }
